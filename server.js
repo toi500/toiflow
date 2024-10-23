141c02fd8cbd4906c75b4b7244d28d1051253db7
toi500 <138339291+toi500@users.noreply.github.com>
2024-10-24 00:00:44 +0200

    Added private network access headers and CORS configuration

--- server.js
+++ server.js
@@ -18,10 +18,19 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(express.static("public"));
 
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Private-Network', 'true');
+  res.sendStatus(200);
+});
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Private-Network', 'true');
   next();
 });
 
@@ -41,7 +50,8 @@ app.get('/api/v1/chatflows-streaming', (req, res) => {
     'Content-Type': 'text/event-stream',
     'Cache-Control': 'no-cache',
     'Connection': 'keep-alive',
-    'Access-Control-Allow-Origin': '*'
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Private-Network': 'true'
   });
   
   res.write('data: connected\n\n');
